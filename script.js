39382eb4702683b7f22eb6b92044c903122e8b21
Hakim NZR <anazary21@posta.pau.edu.tr>
2024-03-03 18:49:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function appendToDisplay(value) {
-    document.getElementById('display').value += value;
+    document.getElementById("display").value += value;
 }
 
 function clearDisplay() {
-    document.getElementById('display').value = '';
+    document.getElementById("display").value = "";
 }
 
 function calculateResult() {
-    var expression = document.getElementById('display').value;
-    var result = eval(expression);
-    document.getElementById('display').value = result;
+    var expression = document.getElementById("display").value;
+    try {
+        var result = eval(expression);
+        document.getElementById("display").value = result;
+    } catch (error) {
+        document.getElementById("display").value = "Error";
+    }
 }
-
